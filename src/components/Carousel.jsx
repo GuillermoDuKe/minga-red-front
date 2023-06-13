447b60221c46851e53b995d15dc2d06ac42fff29
Guillermo Duque <guille980627@gmail.com>
2023-06-13 16:28:25 -0500

    minga-13-14

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,29 +1,64 @@
-// import footer1 from "/img/footer1.png";
-// import footer2 from "/img/footer2.png";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import apiUrl from '../apiUrl'
 import left from "/img/left.svg";
 import right from "/img/right.svg";
 
-export default function Carousel({character_photo,cover_photo,name,description}){
+export default function Carousel(){
+  const [categories,setCategories] = useState([])
+  const [counter,setCounter] = useState(0)
+  useEffect(
+      ()=> {
+          axios(apiUrl+'/categories')
+              .then(res=> {
+                  //console.log(res)
+                  setCategories(res.data.response)
+              })
+              .catch(err=>console.log(err))
+      },
+      []
+  )
+  
+  const next = () => {
+    if (counter == categories.length-1) {
+      setCounter(0);
+    } else {
+      setCounter(counter + 1);
+      console.log(counter);
+    }
+  };
+
+  const prev = () => {
+    if (counter == 0) {
+      setCounter(categories.length-1)
+    }else{
+      setCounter(counter - 1);
+      console.log(counter);
+    }
+  };
+
+
     return(
         <div className="hidden lg:inline-flex lg:mt-[19%] h-[380px] bg-white w-full items-center justify-center">
         <div className="bg-gradient-to-r from-indigo-700 to-indigo-500 flex w-[90%] h-[265px] items-center justify-between rounded-md">
-          <img src={left} className="ps-3" alt="" />
+          <img src={left} className="ps-3 cursor-pointer" alt="Previous" onClick={prev}/>
           <div className="flex mb-12 mr-8">
             <img
-              src={character_photo}
+              src={categories[counter]?.character_photo}
               className="w-[276px] h-[306px] mr-16"
               alt=""
             />
-            <img src={cover_photo} className="w-[180px] h-[270px]" alt="" />
+            <img src={categories[counter]?.cover_photo} className="w-[180px] h-[270px]" alt="" />
           </div>
 
           <div className="text-white w-[25%] h-[93px] flex flex-col justify-center">
-            <h1 className="font-bold text-2xl font-roboto">{name}</h1>
-            <p className="font-roboto text-sm">
-              {description}
+            <h1 className="font-bold text-2xl font-roboto">{categories[counter]?.name}</h1>
+            <p className="font-roboto text-sm color-black ">
+              {categories[counter]?.description}
             </p>
           </div>
-          <img src={right} className="pr-3" alt="" />
+          <img src={right} className="pr-3 cursor-pointer" alt="Next" onClick={next}/>
         </div>
       </div>
     )
