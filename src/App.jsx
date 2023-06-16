7fefe32971f47cac1b22c5e69f5b3e7c2254c118
Guillermo Duque <guille980627@gmail.com>
2023-06-16 18:35:31 -0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import backgroundHome from "/img/fondo.png";
+
 import Index from './pages/Index.jsx'
 import Main from "./layouts/Main.jsx";
 
@@ -13,12 +13,9 @@ function App() {
   ]
   return (
     <>
-      <div className="flex flex-col relative items-center">
-        <img className="lg:absolute w-full lg:h-[100%] lg:object-cover lg:object-top" src={backgroundHome} alt="" />
-        <Main>
-          <Index data={data}/>
-        </Main>
-      </div>
+      <Main>
+        <Index data={data}/>
+      </Main>
     </>
   );
 }
--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,13 +1,17 @@
+import backgroundHome from "/img/fondo.png";
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 
 export default function Main({children}) {
     return (
         <>
-        <NavBar />
-        {/* VISTAS DINÁMICAS */}
-        {children}
-        <Footer />
+            <div className="flex flex-col relative items-center">
+                <img className="lg:absolute w-full lg:h-[100%] lg:object-cover lg:object-top" src={backgroundHome} alt="" />
+                <NavBar />
+                    {/* VISTAS DINÁMICAS */}
+                    {children}
+                <Footer />
+            </div>
         </>
     )
 }
